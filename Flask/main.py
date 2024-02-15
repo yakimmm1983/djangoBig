6f2dc04da29b70054f8fb8b17dbe6a7f780938c4
student <yakimmm1983@yandex.ru>
2024-02-15 19:04:41 +0300

    flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app = Flask("flaskapp")
 
 @app.route("/")
 def HelloFlask():
     return "Hello Flask"
+@app.route("/about")
+def About():
+    return render_template('About.html')
+
+@app.route("/watch",methods =['GET'])
+def Watch():
+    return render_template('watch.html')
+
+@app.route("/submit",methods =['POST'])
+def Submit():
+    username = request.form["username"]
+    return render_template("submit.html",username=username)
+
 if __name__ == "__main__":
     app.run(debug=True)
